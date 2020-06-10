5b6db98f18904ae35b0757a6cab308795d896abe
erictaurone <45673519+erictaurone@users.noreply.github.com>
2020-06-10 13:20:34 -0700

    Add project files.

---
+++ for_loop_practice.js
@@ -0,0 +1,47 @@
+// JavaScript source code
+
+
+function calcTot(merchTot) {
+    var orderTot;
+    if (merchTot >= 100) {
+        orderTot = merchTot;
+    }
+    else if (merchTot < 50.01) {
+        orderTot = merchTot + 5;
+    }
+    else {
+        orderTot = merchTot + 5 + merchTax(merchTot);
+    }
+
+    function merchTax(merchTot) {
+
+        var taxTot = (0.03 * (merchTot - 50))
+
+        return taxTot
+    }
+
+    return orderTot.toFixed(2);
+}
+
+
+function checkAddress(fieldID) {
+    if (document.getElementById(fieldId).value === "") {
+        alert ("email address required.")
+    }
+}
+
+function fillCity() {
+    var cityName;
+    var zipEntered = document.getElementById("zip").value;
+    switch (zipEntered) {
+        case "60608":
+            cityName = "Chicago";
+            break;
+        case "68114":
+            cityName = "Omaha";
+            break;
+        case "53212":
+            cityName = "Milwaukee";
+    }
+    document.getElementById("city").value = cityName;
+}
